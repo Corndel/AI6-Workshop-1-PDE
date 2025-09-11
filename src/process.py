cbda2ef8fd28cc72eb3398ed52caf0e9bf52f553
Æsc George <aesc.george@corndel.com>
2025-09-11 15:51:57 +0100

    Output LIBSVM files instead of CSV

--- src/process.py
+++ src/process.py
@@ -2,6 +2,7 @@ import pandas as pd
 import argparse
 import os
 from sklearn.model_selection import train_test_split
+from sklearn.datasets import dump_svmlight_file
 
 #Corndel Level 6 AI/ML Engineer Workshop 1
 #Data Processing Stage Script
@@ -27,8 +28,5 @@ if __name__ == "__main__":
     y = df['Target']
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=args.train_test_split_ratio, random_state=42, stratify=y)
 
-    train_df = pd.concat([y_train, X_train], axis=1)
-    validation_df = pd.concat([y_val, X_val], axis=1)
-
-    train_df.to_csv(os.path.join(train_output_path, 'train.csv'), index=False, header=False)
-    validation_df.to_csv(os.path.join(validation_output_path, 'validation.csv'), index=False, header=False)
+    dump_svmlight_file(X_train, y_train, os.path.join(train_output_path, 'train.libsvm'), zero_based=True)
+    dump_svmlight_file(X_val, y_val, os.path.join(validation_output_path, 'validation.libsvm'), zero_based=True)
